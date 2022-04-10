8fffe9336182af5a3ec1f9bfb9fb68a721ac3b49
Eric Lopes <nullhack@users.noreply.github.com>
2022-04-11 04:42:23 +0800

    Added redundancy to dev dependencies installation

--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -54,30 +54,35 @@ def install_dev(session):
 @nox.session(python=False, name="tests")
 def tests(session):
     """Run default tests."""
+    install_dev(session)
     session.run("poetry", "run", "pytest")
 
 
 @nox.session(python=False, name="pre-commit")
 def pre_commit(session):
     """Run pre-commit checks."""
+    install_dev(session)
     session.run("poetry", "run", "pre-commit", "run", "--all-files")
 
 
 @nox.session(python=False, name="black")
 def black(session):
     """Fix code structure using black."""
+    install_dev(session)
     session.run("poetry", "run", "black", "src", "tests", "noxfile.py")
 
 
 @nox.session(python=False, name="lint")
 def lint(session):
     """Check styling using Flake8 and external packages."""
+    install_dev(session)
     session.run("poetry", "run", "flake8")
 
 
 @nox.session(python=False, name="uml")
 def uml(session):
     """Generate UML diagrams."""
+    install_dev(session)
     p = Path(r"src").glob("*")
     folders = [x for x in p if x.is_dir()]
     for folder in folders:
@@ -111,6 +116,7 @@ def uml(session):
 @nox.session(python=False, name="test-docs")
 def test_docs(session):
     """Run default tests."""
+    install_dev(session)
     session.run(
         "poetry",
         "run",
@@ -125,6 +131,7 @@ def test_docs(session):
 @nox.session(python=False, name="api-docs")
 def api_docs(session):
     """Generate API documentation using docstrings and pdoc3."""
+    install_dev(session)
     session.run(
         "poetry",
         "run",
