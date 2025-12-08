d06a3cc03e5e07e204119f34f90564c5cdc878c1
eol <nullhack@users.noreply.github.com>
2025-12-07 23:48:26 -0500

    Update main.py

--- {{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/main.py
@@ -1,19 +1,32 @@
 """Test main file."""
 
 import logging
+import fire
 
-logger = logging.Logger(__name__)
-sh = logging.StreamHandler()
-sh.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-sh.setFormatter(formatter)
-logger.addHandler(sh)
+logger = logging.getLogger(__name__)
 
+def set_logging(verbosity: int = 0) -> None:
+    if verbosity >= 3:
+        level = logging.DEBUG
+    elif verbosity == 2:
+        level = logging.INFO
+    elif verbosity == 1:
+        level = logging.WARNING
+    else:
+        level = logging.ERROR
+    logging.basicConfig(
+            level=level,
+            format="%(levelname)s - %(name)s: %(message)s"
+        )
 
-def main() -> None:
-    """Just a main function."""
-    logger.info("Hello from python-project-uv!")
-
+def main(verbosity: int = 0):
+    """Run with --verbosity=N (0..3+)"""
+    set_logging(verbosity)
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+    return "done"
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(main)
